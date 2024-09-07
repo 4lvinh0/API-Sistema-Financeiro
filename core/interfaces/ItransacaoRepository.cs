9ff64e16658fb7d46f3149cd59395c246e1b26df
4lvinh0 <155503937+4lvinh0@users.noreply.github.com>
2024-09-07 00:45:29 -0300

    Update ItransacaoRepository.cs

--- core/interfaces/ItransacaoRepository.cs
+++ core/interfaces/ItransacaoRepository.cs
@@ -8,8 +8,16 @@ namespace Gerenciamento_Financeiro.core.interfaces
 {
     public interface ItransacaoRepository
     {
-        public List<transacao> BuscarPorData();
+        public List<transacao> BuscarPorData(DateOnly DataBusca);
 
-        public void 
+        public List<transacao> BuscarPorTipoDeOperacao(bool tipoOperacao);
+
+        public transacao BuscarPorID(int Id);
+
+        public void AdicionarTransacao(transacao transacao);
+
+        public void ModificarTransacao(transacao transacao);
+
+        public void ExcluirTransacaoPorID(int Id);
     }
 }
