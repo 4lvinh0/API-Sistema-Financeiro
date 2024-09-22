2b7cd5c7fd8bc9c689a70c51a551ab55c62605a9
4lvinh0 <edunascimentoalvess@gmail.com>
2024-09-21 23:30:06 -0300

    Criacao endpoints

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ using Microsoft.EntityFrameworkCore;
 using Gerenciamento_Financeiro.infrastructure.data;
 using Gerenciamento_Financeiro.infrastructure.repositories;
 using Microsoft.AspNetCore.Http.HttpResults;
+using System;
+using Gerenciamento_Financeiro.core.enums;
+using Gerenciamento_Financeiro.application.services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-    //Serviços
+//Serviços
 
 //Dependencias
+builder.Services.AddScoped<transacaoServices>();
 builder.Services.AddScoped<transacaoRepository>();
 
 //Banco de Dados
@@ -34,18 +38,52 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-
-app.MapGet("/", () => "Hello World!");
-
-
+/*
 app.MapPost("/AdicionarTransacao", (Transacao transacao, transacaoRepository teste) =>
-{ 
+{
     teste.AdicionarTransacao(transacao);
     return Results.Ok("Concluida com sucesso");
 });
+*/
 
+app.MapGet("/BuscarPorData", (DateOnly DataBusca, transacaoServices t) =>
+{
+    var result = t.BuscarPorDataService(DataBusca);
+    return Results.Ok(result);
 
+});
 
+app.MapGet("/BuscarPorTipoDeOperacao", (tipoOperacao tipoOperacao, transacaoServices t) =>
+{
+    var result = t.BuscarPorTipoDeOperacaoService(tipoOperacao);
+    return Results.Ok(result);
+
+});
+
+app.MapGet("/BuscarPorID", (int Id, transacaoServices t) =>
+{
+    var result = t.BuscarPorIDService(Id);
+    return Results.Ok(result);
+
+});
+
+app.MapPost("/AdicionarTransacao", (Transacao transacao, transacaoServices t) =>
+{
+    t.AdicionarTransacaoService(transacao);
+    return Results.Ok("Concluida com sucesso");
+});
+
+app.MapPut("/ModificarTransacao", (Transacao transacao, transacaoServices t) =>
+{
+    t.ModificarTransacaoService(transacao);
+    return Results.Ok("Concluida com sucesso");
+});
+
+app.MapDelete("/RemoverPorID", (int Id, transacaoServices t) =>
+{
+    t.ExcluirTransacaoPorIDService(Id);
+    return Results.Ok("Concluida com sucesso");
+
+});
 
 app.Run();
